kinda fixed the problem with the interpolation implemented a new filter into the testangles function, this works way better than checking the signs but still needs improvements

init.py
@@ -4,6 +4,7 @@ import scipy.interpolate as spint
 import scipy.spatial.qhull as qhull
 import itertools
 
+
 class Square:
 
     def __init__(self, cRange, maxima, status, rainThreshold, distThreshold, dist, id):
@@ -169,6 +170,16 @@ class totalField:
         maxima = maxima[maximaProx > self.rainThreshold, :]
         status = status[maximaProx > self.rainThreshold]
 
+        status=list(status)
+
+        for i, field in enumerate(self.activeFields):
+            if field.lifeTime > 1:
+                if (field.maxima[0, 1:3] == field.histMaxima[-1][0, 1:3]).all():
+                    try:
+                        status.remove(i)
+                    except ValueError:
+                        pass
+
         for i in reversed(range(len(self.activeFields))):
             if i not in status:
                 self.inactiveFields.append(self.activeFields[i])
@@ -218,7 +229,8 @@ class totalField:
             if field.lifeTime < self.trainTime:
                 self.activeFields.remove(field)
 
-        filter = set()
+        filter = []
+        extraFilter = set()
         for t in range(self.trainTime):
             shiftX = np.empty([len(self.activeFields)])
             shiftY = np.empty([len(self.activeFields)])
@@ -232,6 +244,7 @@ class totalField:
 
             shiftXex = shiftX[lengths <= 800]
             shiftYex = shiftY[lengths <= 800]
+            extraFilter = extraFilter.union(set(status[lengths > 800]))
             status = status[lengths <= 800]
 
             meanXex = np.empty([len(shiftXex)])
@@ -241,16 +254,22 @@ class totalField:
                 meanXex[i] = np.mean(np.delete(shiftXex, i))
                 meanYex[i] = np.mean(np.delete(shiftYex, i))
 
-            #shiftX = shiftXex[(np.sign(meanXex) == np.sign(shiftXex)) | (np.sign(meanYex) == np.sign(shiftYex))]
-            #shiftY = shiftYex[(np.sign(meanXex) == np.sign(shiftXex)) | (np.sign(meanYex) == np.sign(shiftYex))]
-            status = status[(np.sign(meanXex) == np.sign(shiftXex)) | (np.sign(meanYex) == np.sign(shiftYex))]
+            # shiftX = shiftXex[(np.sign(meanXex) == np.sign(shiftXex)) | (np.sign(meanYex) == np.sign(shiftYex))]
+            # shiftY = shiftYex[(np.sign(meanXex) == np.sign(shiftXex)) | (np.sign(meanYex) == np.sign(shiftYex))]
+            # status = status[(np.sign(meanXex) == np.sign(shiftXex)) | (np.sign(meanYex) == np.sign(shiftYex))]
+            angleEx = get_metangle(meanXex, meanYex)
+            angle = get_metangle(shiftXex, shiftYex)
+            a = 180 - np.abs(np.abs(angle - angleEx) - 180)
 
-            filter = filter.union(set(np.arange(len(self.activeFields))) - set(status))
+            status = status[a<45]
+            filter.extend(list(status))
 
-        filter = sorted(list(filter))
+        unique, counts = np.unique(np.array(filter), return_counts=True)
+
+        filter = (np.full_like(counts, self.trainTime) - counts) >= 3
 
         for i in reversed(range(len(self.activeFields))):
-            if i in filter:
+            if filter[i]:
                 self.inactiveFields.append(self.activeFields[i])
                 self.inactiveFields[-1].lifeTime = -1
                 del self.activeFields[i]
@@ -283,5 +302,5 @@ def interp_weights(xy, uv, d=2):
 
 def interpolate(values, vtx, wts, fill_value=np.nan):
     ret = np.einsum('nj,nj->n', np.take(values, vtx), wts)
-    ret[np.any(wts < 0, axis=1)] = fill_value
+    #ret[np.any(wts < 0, axis=1)] = fill_value
     return ret

rainprog.py
@@ -30,12 +30,13 @@ def gauss(x, *p):
 
 
 #fp = 'C:/Rainprog/m4t_BKM_wrx00_l2_dbz_v00_20130511160000.nc'
-fp = '/home/zmaw/u300675/pattern_data/m4t_BKM_wrx00_l2_dbz_v00_20130511160000.nc'
+fp = '/home/zmaw/u300675/pattern_data/m4t_BKM_wrx00_l2_dbz_v00_20130426120000.nc'
+#fp = '/home/zmaw/u300675/pattern_data/m4t_BKM_wrx00_l2_dbz_v00_20130426120000.nc' difficult field to predict
 res = 100
 smallVal = 2
 rainThreshold = 0.1
 distThreshold = 17000
-prog = 60
+prog = 16
 trainTime = 8
 numMaxes = 20
 progTime = 20
@@ -48,17 +49,15 @@ nc = netCDF4.Dataset(fp)
 data = nc.variables['dbz_ac1'][:][:][:]
 z = data
 azi = nc.variables['azi'][:]
-dist = nc.variables['range'][:]
+rang = nc.variables['range'][:]
 
 aziCos = np.cos(np.radians(azi))
 aziSin = np.sin(np.radians(azi))
-xPolar = np.outer(dist, aziCos)
+xPolar = np.outer(rang, aziCos)
 xPolar = np.reshape(xPolar, (333*360, 1))
-yPolar = np.outer(dist, aziSin)
+yPolar = np.outer(rang, aziSin)
 yPolar = np.reshape(yPolar, (333*360, 1))
 points = np.concatenate((xPolar, yPolar), axis = 1)
-meanX = np.zeros([prog])
-meanY = np.zeros([prog])
 
 xCar = np.arange(-20000, 20000+1, res).squeeze()
 yCar = np.arange(-20000, 20000+1, res).squeeze()
@@ -85,15 +84,16 @@ if useRealData:
     for t in range(timeSteps):
         rPolarT = rPolar[t, :, :].T
         rPolarT = np.reshape(rPolarT, (333*360, 1)).squeeze()
-        R[t, :, :] = np.reshape(interpolate(rPolarT, vtx, wts), (d_s, d_s))
-        R[t, (dist > 20000)] = 0
+        R[t, :, :] = np.reshape(interpolate(rPolarT.flatten(), vtx, wts), (d_s, d_s))
+        R[t, (dist >= np.max(rang))] = 0
         nestedData[t, 2 * cRange: 2 * cRange + d_s, 2 * cRange: 2 * cRange + d_s] = R[t, :, :]
 else:
     R = createblob(d_s, res, timeSteps)
     R[:, (dist > 20000)] = 0
     nestedData[:, 2 * cRange: 2 * cRange + d_s, 2 * cRange: 2 * cRange + d_s] = R
 
-
+print(np.sum(np.isnan(rPolarT)))
+print(np.sum(np.isnan(R)))
 time_elapsed = datetime.now() - startTime
 print(time_elapsed)
 nestedData = np.nan_to_num(nestedData)
@@ -166,6 +166,15 @@ for t in range(prog):
     plt.pause(0.01)
 
     allFields.update_fields()
+
+plt.figure(figsize=(8, 8))
+for field in allFields.activeFields:
+    for t in field.histMaxima:
+        plt.plot(*np.transpose(t[0][2:0:-1]), 'ko')
+for field in allFields.inactiveFields:
+    for t in field.histMaxima:
+        plt.plot(*np.transpose(t[0][2:0:-1]), 'ro')
+plt.show(block=False)
 
 allFields.testangles()
 