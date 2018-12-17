fbb4c4654fc1c12f9d579e093c8c5202a9ad6113
smichel <simon.michel.j@gmail.com>
2018-12-17 13:13:55 +0100

    addition of the figures

--- plotting.py
+++ plotting.py
@@ -1,21 +1,27 @@
 import matplotlib.patches as mpatches
 
 import matplotlib.colors as colors
-contours = [0, 0.1, 0.2, 0.5, 1, 2, 5, 10, 20, 50, 100]
+contours = [0, 0.1, 0.5, 1, 2, 5, 10, 20, 50, 100]
 
-fig, ax = plt.subplots(1)
+def truncate_colormap(cmap, minval=0.0, maxval=1.0, n=100):
+    new_cmap = colors.LinearSegmentedColormap.from_list(
+        'cmaptest',cmap(np.linspace(minval, maxval, n)))
+    return new_cmap
 cmap = plt.get_cmap('BuPu')
 newcmap=truncate_colormap(cmap,0.2,1)
 newcmap.set_under('1')
+
+fig, ax = plt.subplots(1)
+
 dataArea1 =self.nested_data[t+1,
                         (int(field.maxima[0, 1]) - self.cRange * 2):(int(field.maxima[0, 1]) + self.cRange * 2),
                         (int(field.maxima[0, 2]) - self.cRange * 2):(int(field.maxima[0, 2]) + self.cRange * 2)]
-dat = ax.imshow(dataArea1, norm=matplotlib.colors.SymLogNorm(vmin=0, linthresh=1))
-plt.plot(48,48,marker='X',color='ghostwhite',markersize=8)
+dat = ax.imshow(dataArea1, norm=matplotlib.colors.SymLogNorm(vmin=0, linthresh=1),cmap=newcmap)
+plt.plot(48,48,marker='X',color='red',markersize=8)
 plt.plot(48+int(cIdx[1] - 0.5 * len(c)),48+int(cIdx[0] - 0.5 * len(c)),marker='X',color='black',markersize=8)
 s = plt.colorbar(dat, format=matplotlib.ticker.ScalarFormatter())
 s.set_label('Precipitation in mm/h')
-s.set_clim(0, 100)
+s.set_clim(0.1, 100)
 s.set_ticks(contours)
 s.draw_all()
 ax.grid(linewidth=0.5)
@@ -24,18 +30,16 @@ ax.set_yticklabels(ax.get_yticks()*0.25)
 plt.show()
 
 fig, ax = plt.subplots(1)
-cmap = plt.get_cmap('viridis')
-cmap.colors[0] = [0.75, 0.75, 0.75]
 dataArea1 =self.nested_data[t,
                        (int(field.maxima[0, 1]) - self.cRange * 2):(int(field.maxima[0, 1]) + self.cRange * 2),
                        (int(field.maxima[0, 2]) - self.cRange * 2):(int(field.maxima[0, 2]) + self.cRange * 2)]
-dat = ax.imshow(dataArea1, norm=matplotlib.colors.SymLogNorm(vmin=0, linthresh=1))
+dat = ax.imshow(dataArea1, norm=matplotlib.colors.SymLogNorm(vmin=0, linthresh=1),cmap=newcmap)
 corrAreaRect = mpatches.Rectangle((24,24),48,48,color='r',linewidth=1,fill=0)
 ax.add_patch(corrAreaRect)
-plt.plot(48,48,marker='X',color='ghostwhite',markersize=8)
+plt.plot(48,48,marker='X',color='red',markersize=8)
 s = plt.colorbar(dat, format=matplotlib.ticker.ScalarFormatter())
 s.set_label('Precipitation in mm/h')
-s.set_clim(0, 100)
+s.set_clim(0.1, 100)
 s.set_ticks(contours)
 s.draw_all()
 ax.set_xticklabels(ax.get_xticks()*0.25)
@@ -103,37 +107,35 @@ gl2.ylabels_left = False
 
 plt.show()
 
-def truncate_colormap(cmap, minval=0.0, maxval=1.0, n=100):
-    new_cmap = colors.LinearSegmentedColormap.from_list(
-        'cmaptest',cmap(np.linspace(minval, maxval, n)))
-    return new_cmap
 
 
+plt.rcParams.update({'font.size': 22})
+t = 0
+fig, ax = plt.subplots(1, figsize=(10, 8))
+im = ax.imshow(dwd.nested_data[0, :, :], norm=matplotlib.colors.SymLogNorm(vmin=0, linthresh=1), cmap=newcmap)
+plt.show(block=False)
+o, = plt.plot(*np.transpose(dwd.progField.return_maxima(t)[:, 2:0:-1]), 'rX')
+s = plt.colorbar(im, format=matplotlib.ticker.ScalarFormatter())
+s.set_clim(0.1, 100)
+s.set_ticks(contours)
+ax.set_xlim([50,448])
+ax.set_ylim([448,50])
+ax.grid(linewidth=0.5)
+s.draw_all()
+ax.set_xticklabels((ax.get_xticks()-50) * 0.25)
+ax.set_yticklabels((ax.get_yticks()-50) * 0.25)
 
 t = 0
 fig, ax = plt.subplots(1, figsize=(10, 8))
 im = ax.imshow(dwd.nested_data[0, :, :], norm=matplotlib.colors.SymLogNorm(vmin=0, linthresh=1), cmap=newcmap)
 plt.show(block=False)
-o, = plt.plot(*np.transpose(dwd.progField.return_maxima(t)[:, 2:0:-1]), 'wX')
-s = plt.colorbar(im, format=matplotlib.ticker.ScalarFormatter())
-s.set_clim(0.1, 100)
-s.set_ticks(contours)
-ax.grid(linewidth=0.5)
-s.draw_all()
-ax.set_xticklabels(ax.get_xticks() * 0.25)
-ax.set_yticklabels(ax.get_yticks() * 0.25)
-
-t = 0
-fig, ax = plt.subplots(1, figsize=(10, 8))
-im = ax.imshow(dwd.nested_data[0, :, :], norm=matplotlib.colors.SymLogNorm(vmin=0, linthresh=1), cmap=cmap)
-plt.show(block=False)
-o, = plt.plot(*np.transpose(dwd.progField.return_maxima(t)[:, 2:0:-1]), 'wX')
+o, = plt.plot(*np.transpose(dwd.progField.return_maxima(t)[:, 2:0:-1]), 'rX')
 maxima = dwd.progField.return_maxima(t)[:, 2:0:-1]
 for u in range(len(maxima)):
     proxCircle = mpatches.Circle((maxima[u,0],maxima[u,1]),3000/dwd.resolution,color='r',linewidth=1,fill=0)
     ax.add_patch(proxCircle)
 s = plt.colorbar(im, format=matplotlib.ticker.ScalarFormatter())
-s.set_clim(0, 100)
+s.set_clim(0.1, 100)
 s.set_ticks(contours)
 ax.set_xlim([100,300])
 ax.set_ylim([300,100])
@@ -141,7 +143,7 @@ ax.grid(linewidth=0.5)
 s.draw_all()
 ax.set_xticklabels(ax.get_xticks() * 0.25)
 ax.set_yticklabels(ax.get_yticks() * 0.25)
-matplotlib.rcParams.update({'axes.size': 20})
+plt.rcParams.update({'font.size': 22})
 
 
 col = np.concatenate(
